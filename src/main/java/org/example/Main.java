e625059a872e99b714c69e0ac245503bcec00a7b
vaguewords <vaguewords@gmail.com>
2023-08-03 19:12:56 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,13 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("제발 되라..!!!");
-
+        System.out.println("제발 되라..!!!");
+        System.out.println("She's got a smile that it seems to me\n" +
+                "Reminds me of childhood memories\n" +
+                "Where everything was as fresh as the bright blue sky\n" +
+                "Now and then when I see her face\n" +
+                "She takes me away to that special place\n" +
+                "And if I stare too long, I'd probably break down and cry");
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
